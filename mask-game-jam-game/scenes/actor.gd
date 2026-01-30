887367b69f032f0071a207c5b406325ec623d583
godzilla5hrimp <oleg.a.lavrov@protonmail.com>
2026-01-31 00:16:13 +0100

    feature: add collision logic;

--- mask-game-jam-game/scenes/actor.gd
+++ mask-game-jam-game/scenes/actor.gd
@@ -3,15 +3,17 @@ var healthbar = 100
 var live = 3
 @export var speed = 400
 var screen_size
+signal hit
 
 func _ready():
+	position = Vector2(100, 100);
 	return
 	
 func _physics_process(delta: float):
-	checkInput(delta)
 	screen_size = get_viewport().get_visible_rect().size
+	checkInput(delta)
 	
-func checkInput(delta: float):
+func checkInput(_delta: float):
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("moveUp"):
 		velocity.y -=1 
@@ -25,4 +27,6 @@ func checkInput(delta: float):
 		velocity = velocity.normalized() * speed
 	move_and_slide()
 	#position += velocity * delta
-	#position = get_node(".").position.clamp(Vector2.ZERO, screen_size)
+#	todo: remove that, Ralph is handling out of bounds collision logic
+	var collision_shape_rect = get_node("%CollisionMovementShape").shape.get_rect()
+	position = position.clamp(collision_shape_rect.position, collision_shape_rect.end)
