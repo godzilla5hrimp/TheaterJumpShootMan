a89e1b5e9797c755706df45ee6590990812308bf
godzilla5hrimp <oleg.a.lavrov@protonmail.com>
2026-01-30 23:45:31 +0100

    fix: fix player movement;

--- mask-game-jam-game/scenes/actor.gd
+++ mask-game-jam-game/scenes/actor.gd
@@ -1,27 +1,28 @@
 extends CharacterBody2D
 var healthbar = 100
 var live = 3
-@export var speed = 200
+@export var speed = 400
 var screen_size
 
 func _ready():
 	return
 	
-func _process(delta):
+func _physics_process(delta: float):
 	checkInput(delta)
 	screen_size = get_viewport().get_visible_rect().size
 	
 func checkInput(delta: float):
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("moveUp"):
-		velocity.y -=delta
-	elif Input.is_action_pressed("moveDown"):
-		velocity.y +=delta
-	elif Input.is_action_pressed("moveLeft"):
-		velocity.x -=delta
-	elif Input.is_action_pressed("moveRight"):
-		velocity.x +=delta
+		velocity.y -=1 
+	if Input.is_action_pressed("moveDown"):
+		velocity.y +=1
+	if Input.is_action_pressed("moveLeft"):
+		velocity.x -=1
+	if Input.is_action_pressed("moveRight"):
+		velocity.x +=1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	move_and_slide()
-	#get_node(".").position += velocity * delta
-	#get_node(".").position = get_node(".").position.clamp(Vector2.ZERO, screen_size)
+	#position += velocity * delta
+	#position = get_node(".").position.clamp(Vector2.ZERO, screen_size)
