101b8e7ca0dcb3e357cd8653b5957567b1cc5831
Tunahead <ralf.speer@mailbox.org>
2026-01-30 22:16:00 +0100

    removed .kra file, oops

---
+++ mask-game-jam-game/scenes/main.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _process(_delta: float):
+	if Input.is_action_just_pressed("close_button"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		get_tree().change_scene("res://scenes/level_base.tscn")
