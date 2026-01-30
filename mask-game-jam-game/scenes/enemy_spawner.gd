c7e80c36f8e0ae04a368a5e3a4928481691423a9
SirTunahead <ralf.speer@mailbox.org>
2026-01-31 00:50:47 +0100

    basic enemy spawner

--- mask-game-jam-game/scenes/enemy_spawner.gd
+++ mask-game-jam-game/scenes/enemy_spawner.gd
@@ -1,1 +1,18 @@
 extends Node2D
+
+@export var enemy_scene: PackedScene
+var left = true
+
+func spawn_enemy():
+	var enemy = enemy_scene.instantiate()
+	
+	if left:
+		enemy.global_position = Vector2(-140.0, 920.0)
+		left = false
+	elif left == false:
+		enemy.global_position = Vector2(2053.0, 920.0)
+		left = true
+	get_tree().root.get_node("Main").add_child(enemy)
+
+func _on_timer_timeout() -> void:
+	spawn_enemy()
