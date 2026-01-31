856a9a5e0515d1714610cadb9bcfe8322e9b3bb3
ninja666gaming <ninja666gaming@gmail.com>
2026-01-31 03:55:39 -0700

    hearts ui

--- mask-game-jam-game/ui.gd
+++ mask-game-jam-game/ui.gd
@@ -1,5 +1,5 @@
 extends Control
-
+var HeartAmount = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +9,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_playertest_heart_loss() -> void:
+	HeartAmount -=1 
+	print (HeartAmount)
+	if HeartAmount == 2:
+		$HeartsUI/Hp3.hide()
+	if HeartAmount == 1:
+		$HeartsUI/Hp2.hide()
+	if HeartAmount == 0:
+		$HeartsUI/Hp1.hide()
