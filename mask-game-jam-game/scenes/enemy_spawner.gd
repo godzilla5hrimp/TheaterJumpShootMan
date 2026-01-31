95ffb9f5efd3ecbaa88dd4ebba6ce75c7e7a0be1
SirTunahead <ralf.speer@mailbox.org>
2026-01-31 19:42:04 +0100

    enemy spawn now fit more to level

--- mask-game-jam-game/scenes/enemy_spawner.gd
+++ mask-game-jam-game/scenes/enemy_spawner.gd
@@ -17,20 +17,24 @@ func spawn_enemy():
 		else:
 			enemy.shooter = false
 	var choose_pos = rng.randi_range(0,2) 
-	print(choose_pos)
+	
 	if left:
 		if choose_pos == 0:
-			enemy.global_position = Vector2(-140.0, 400.0)
-		if choose_pos == 1:
 			enemy.global_position = Vector2(-140.0, 600.0)
+		if choose_pos == 1:
+			enemy.global_position = Vector2(-140.0, 700.0)
 		if choose_pos == 2:
 			enemy.global_position = Vector2(-140.0, 800.0)
+
 		left = false
 	elif left == false:
-		enemy.global_position = Vector2(2053.0, 800.0)
+		if choose_pos == 0:
+			enemy.global_position = Vector2(2053.0, 600.0)
+		if choose_pos == 1:
+			enemy.global_position = Vector2(2053.0, 700.0)
+		if choose_pos == 2:
+			enemy.global_position = Vector2(2053.0, 800.0)
 		left = true
 	get_tree().root.get_node("Main/LevelManager").add_child(enemy)
-	print(enemy.global_position)
-
 func _on_timer_timeout() -> void:
 	spawn_enemy()
