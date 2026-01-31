1ae25ffb74bacfa24a3d11f5de5cce1820cde8ec
SirTunahead <ralf.speer@mailbox.org>
2026-01-31 21:29:52 +0100

    enemy shoots in bursts, new music

--- mask-game-jam-game/scenes/basic_enemy.gd
+++ mask-game-jam-game/scenes/basic_enemy.gd
@@ -4,8 +4,11 @@ extends CharacterBody2D
 @export var melee_scene: PackedScene
 @export var shooter = false
 
-const SPEED = 300.0
+var speed = 300.0
 var dir = 0
+
+var shooting = false
+var shoot_count = 0
 
 func _ready():
 	if position.x <= 0:
@@ -16,26 +19,48 @@ func _ready():
 
 
 func _physics_process(delta: float) -> void:
-	velocity.x = dir * SPEED
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	velocity.x = dir * speed
+	if shooting:
+		shoot_bullet()
 	move_and_slide()
 
+func shoot_bullet():
+	if shoot_count <= 3:
+		var attack = bullet_scene.instantiate()
+		attack.global_position = position
+		attack.dir = dir
+		get_tree().root.get_node("Main/LevelManager").add_child(attack)
+		get_tree().root.get_node("Main/MusicManager/EnemyBullet").play()
+		shoot_count += 1
 
 func _on_despawn_timeout() -> void:
 	queue_free()
 
 
 func _on_attack_timer_timeout() -> void:
+	speed = 0
+	print($ShooterTimer.is_stopped())
 	if shooter:
-		var attack = bullet_scene.instantiate()
-		attack.global_position = position
-		attack.dir = dir
-		get_tree().root.get_node("Main/LevelManager").add_child(attack)
-		get_tree().root.get_node("Main/MusicManager/EnemyBullet").play()
+		shooting = true
+		$ShooterTimer.start()
 	elif shooter == false:
+		shooting = false
+		$WaitTimerMelee.start()
 		var attack = melee_scene.instantiate()
 		attack.global_position = position
 		attack.global_position.x = attack.global_position.x + (dir*150)
 		get_tree().root.get_node("Main/LevelManager").add_child(attack)
 		get_tree().root.get_node("Main/MusicManager/EnemyMelee").play()
+
+func _on_wait_timer_melee_timeout() -> void:
+	speed = 300.0
+
+
+func _on_shooter_timer_timeout() -> void:
+	shooting = false;
+	shoot_count = 0
+	speed = 300.0
+
+
+func _on_distance_timer_timeout() -> void:
+	pass # Replace with function body.
