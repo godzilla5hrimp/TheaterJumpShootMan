eaa0e2f7404952fce4f63367fef4539d5d3330c3
SirTunahead <ralf.speer@mailbox.org>
2026-02-01 03:21:50 +0100

    fixed shield

--- mask-game-jam-game/scenes/player_bullet.gd
+++ mask-game-jam-game/scenes/player_bullet.gd
@@ -12,6 +12,6 @@ func _on_clean_up_timeout() -> void:
 	pass # Replace with function body.
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	#print(area.get_parent().name)
+	print(area.get_parent().name)
 	if area.get_parent().name == "BasicEnemy":
 		queue_free()
--- mask-game-jam-game/scenes/player_shield.gd
+++ mask-game-jam-game/scenes/player_shield.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+var type = "Shield"
 
 func _on_clean_up_timeout() -> void:
 	queue_free()
