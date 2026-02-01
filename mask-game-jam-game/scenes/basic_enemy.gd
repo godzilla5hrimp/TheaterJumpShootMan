28e408f8d5f49df25a62d6623948a15cebe0356d
SirTunahead <ralf.speer@mailbox.org>
2026-02-01 12:11:54 +0100

    fixed layering on puppet stage

--- mask-game-jam-game/scenes/basic_enemy.gd
+++ mask-game-jam-game/scenes/basic_enemy.gd
@@ -52,6 +52,7 @@ func _on_attack_timer_timeout() -> void:
 		var attack = bullet_scene.instantiate()
 		attack.global_position = position
 		attack.dir = dir
+		attack.z_index = z_index
 		get_tree().root.get_node("Main/LevelManager").add_child(attack)
 		get_tree().root.get_node("Main/MusicManager/EnemyBullet").play()
 		shooting = true
@@ -63,6 +64,7 @@ func _on_attack_timer_timeout() -> void:
 		var attack = melee_scene.instantiate()
 		attack.global_position = position
 		attack.global_position.x = attack.global_position.x + (dir*150)
+		attack.z_index = z_index
 		get_tree().root.get_node("Main/LevelManager").add_child(attack)
 		get_tree().root.get_node("Main/MusicManager/EnemyMelee").play()
 
