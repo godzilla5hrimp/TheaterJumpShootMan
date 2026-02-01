323ebde30ce9b7a215751ab5e1f28586b2c16fd4
SirTunahead <ralf.speer@mailbox.org>
2026-02-01 15:31:01 +0100

    changed player bullet

--- mask-game-jam-game/scenes/player_bullet.gd
+++ mask-game-jam-game/scenes/player_bullet.gd
@@ -4,6 +4,10 @@ var type = "PlayerBullet"
 const SPEED = 600.0
 @export var dir = 1
 
+func _ready():
+	if dir <= 0:
+		$Sprite2D.flip_h = true
+
 func _physics_process(delta: float) -> void:
 	velocity.x = dir * SPEED
 	move_and_slide()
